6d236a5abcefe7897c5230f8bda2820aff5e34e3
Tienstra <msz.tienstra@gmail.com>
2024-10-01 19:42:20 +0200

    fixed spelling

--- EKI/stopping_rules.py
+++ EKI/stopping_rules.py
@@ -2,22 +2,28 @@ import numpy as np
 
 
 class StoppingRule: 
-    def __init__(self, oberservations, predictions, tolerance, kappa):
-        self.oberservations = oberservations
+    def __init__(self, observations, predictions, tolerance, kappa,max_iterations ):
+        self.observations = observations
         self.predictions = predictions
         self.dim_predictions = np.len(predictions)
         self.tolerance = tolerance 
-        self.kappa
-        self.rediduals = self.compute_residulas()
+        self.kappa = kappa 
+        self.residuals = self.compute_residuals()
+        self.iteration = 0
+        self.max_iterations = max_iterations
+        self.converge = False
 
-    def compute_residulas(self):
-        return np.linalg.norm(self.oberservations - self.predictions)
+
+    def compute_residuals(self):
+        return np.linalg.norm(self.observations - self.predictions)
     
 
 class DiscrepencyPrinciple(StoppingRule):
-    def __init__(self, oberservations, predictions,tolerance, kappa):
-        super().__init__(oberservations, predictions,tolerance, kappa)
+    def __init__(self, observations, predictions, tolerance, kappa,max_iterations):
+        super().__init__(observations, predictions,tolerance, kappa, max_iterations)
 
-    def stop(self):
-         if self.rediduals <= self.kappa*np.sqrt(self.dim_predictions*self.tolerance):
-            False
+    def check_convergence(self):
+         self.iteration +=1
+         if self.residuals <= self.kappa*np.sqrt(self.dim_predictions*self.tolerance):
+            self.converge = True
+
