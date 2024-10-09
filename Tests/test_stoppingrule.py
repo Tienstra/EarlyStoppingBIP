d02621009bae0453219f4f7f1d7237cd68357659
Tienstra <msz.tienstra@gmail.com>
2024-10-09 15:33:06 +0200

    Added Test for DP

--- Tests/test_stoppingrule.py
+++ Tests/test_stoppingrule.py
@@ -5,10 +5,19 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from EKI.stopping_rules import DiscrepancyPrinciple
 
 def test_dp(): 
-    residuals = np.logspace(-1, -5, 5)
-    tol = 1e-3
-    kappa = 1
-    max_iter = 5
 
-    assert DiscrepancyPrinciple(residuals, tol, kappa, max_iter) == True 
+    residuals = 0.02
+    tol = 1
+    kappa = 1
+    max_iter = 4
+    eff_dim = 4
+    
+
+    assert DiscrepancyPrinciple(residuals,eff_dim, tol, kappa, max_iter).converged() == True 
+  
+  
+
+
+
+
     
