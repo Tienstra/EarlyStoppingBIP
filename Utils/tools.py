6804a47bbb0c9e68b8d0a47256ed3508fb02fd62
Tienstra <msz.tienstra@gmail.com>
2025-05-06 15:36:21 +0200

    add funcs

--- Utils/tools.py
+++ Utils/tools.py
@@ -1,4 +1,5 @@
 import jax.numpy as jnp
+from jax import vmap, random, jit
 
 
 def kth_diag_indices(a, k):
@@ -64,6 +65,59 @@ def compute_laplace(D, h):
     Lap = Lap / (2 * (h**2))
 
     return Lap
+
+
+def get_function(w: jnp.ndarray, x: jnp.ndarray) -> jnp.ndarray:
+    """
+    Reconstruct function(s) f(x) = ∑ w_k * φ_k(x) using Laplacian eigenfunctions on [0, 1].
+
+    Parameters:
+    ----------
+    w : jnp.ndarray
+        Coefficients in the Laplace eigenfunction basis.
+        Shape: (m,) for single particle or (m, n_particles) for ensemble.
+    x : jnp.ndarray
+        Points at which to evaluate the function (shape: [M,], values in [0, 1])
+    plot : bool
+        If True and w is a single vector, plot the reconstructed function.
+
+    Returns:
+    -------
+    f : jnp.ndarray
+        Reconstructed function(s) evaluated at points x.
+    """
+
+    def single_reconstruction(w_single):
+        k_vals = jnp.arange(1, len(w_single) + 1)  # shape (m,)
+        Phi = jnp.sqrt(2) * jnp.sin(jnp.outer(k_vals, jnp.pi * x))  # shape (m, M)
+        return jnp.dot(w_single, Phi)  # shape (M,)
+
+    if w.ndim == 1:
+        f = single_reconstruction(w)  # shape (M,)
+        return f
+
+    else:
+        # w: (m, n_particles) → transpose → (n_particles, m)
+        batched_eval = vmap(single_reconstruction, in_axes=(1,), out_axes=1)
+        f_all = batched_eval(w)  # shape: (n_particles, M)
+        return f_all  # shape: (M, n_particles)
+def solution_map(v: jnp.ndarray, K: jnp.ndarray, g_tilde: jnp.ndarray) -> jnp.ndarray:
+    """ """
+
+    def solution_map_single(v, K, g_tilde):
+        denominator = jnp.dot(K, v) + g_tilde
+        return v / 2 * (denominator)
+
+    if v.ndim == 1:
+        f = solution_map_single(v, K, g_tilde)  # shape (M,)
+        return f
+
+    else:
+        # w: (m, n_particles) → transpose → (n_particles, m)
+        batched_eval = vmap(solution_map_single, in_axes=(1, None, None), out_axes=1)
+        f_all = batched_eval(v, K, g_tilde)  # shape: (n_particles, M)
+        return f_all  # shape: (M, n_particles)
+
 
 
 if __name__ == "__main__":
